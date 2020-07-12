c2685dae045c51d983546debd93b29e2d38774ee
NeeleST <neele.sofie@gmail.com>
2020-07-12 19:22:36 +0200

    interface, quest 1 text
    when object is collected a text on canvas pops up

--- Assets/FPS/Scripts/PlayerWeaponsManager.cs
+++ Assets/FPS/Scripts/PlayerWeaponsManager.cs
@@ -66,7 +66,7 @@ public class PlayerWeaponsManager : MonoBehaviour
     public UnityAction<WeaponController, int> onAddedWeapon;
     public UnityAction<WeaponController, int> onRemovedWeapon;
 
-    WeaponController[] m_WeaponSlots = new WeaponController[12]; // 12 available weapon slots
+    WeaponController[] m_WeaponSlots = new WeaponController[16]; // 16 available weapon slots
     PlayerInputHandler m_InputHandler;
     PlayerCharacterController m_PlayerCharacterController;
     float m_WeaponBobFactor;
